7589783452ac6ce2d5f4c564a51e62db8baa9939
Rohit <rohitmondalpaligram4@gmail.com>
2025-09-15 15:25:48 +0530

    Seperated js and css files

---
+++ javafiles/previewpage.js
@@ -0,0 +1,33 @@
+const clickMsg = document.getElementById("clickMessage");
+  const music = document.getElementById("bgMusic");
+  const image = document.getElementById("bg-video");
+  const leftHalf = document.getElementById("left-half");
+  const rightHalf = document.getElementById("right-half");
+  let hasClicked = false;
+  document.body.addEventListener("click", () => {
+
+     document.getElementById("bg-video").style.display = "block";
+    clickMsg.style.display = "none";
+
+    // 1. Start animation
+    leftHalf.classList.add("animate-left");
+    rightHalf.classList.add("animate-right");
+
+    // 2. Flash effect after slight delay
+    setTimeout(() => {
+      const flash = document.createElement("div");
+      flash.classList.add("flash");
+      document.body.appendChild(flash);
+      flash.addEventListener("animationend", () => flash.remove());
+    }, 1000);
+
+    // 3. Play music
+    music.play().catch(() => {
+      console.log("Music autoplay failed.");
+    });
+
+    // 4. Redirect to birthday.html
+     setTimeout(() => {
+       window.location.href = "birthday.html";
+     }, 3000); // Adjust if needed
+  });
